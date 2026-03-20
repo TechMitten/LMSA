3a90392b80d8b4860224e66ff86df8f4bc3dc6e2
Island Applications <techray@outlook.com>
2026-03-20 09:56:37 -0400

    cleanup old files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.lmsa.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 239
+        versionCode = 240
         versionName = "10.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
