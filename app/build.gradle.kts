74bad27255be92c5d0b9c31124460cbdef8b216f
Island Applications <techray@outlook.com>
2026-03-24 11:20:46 -0400

    version nudge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.lmsa.app"
         minSdk = 23
         targetSdk = 35
-        versionCode = 247
-        versionName = "10.3.1"
+        versionCode = 248
+        versionName = "10.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
