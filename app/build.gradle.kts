cfcc967b8ef20624f1384803f0966aa4bd10fe1d
Island Applications <techray@outlook.com>
2026-05-01 17:25:39 -0400

    fix ui bug in modals

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 23
         // Target 36 to stay current with Play Store 2026 requirements
         targetSdk = 36
-        versionCode = 311
+        versionCode = 314
         versionName = "10.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
