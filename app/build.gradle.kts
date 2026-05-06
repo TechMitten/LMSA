d409f2a50eeecf1b855de33f4987153f613b6e87
IslandAPps <techray@outlook.com>
2026-05-06 03:21:14 -0400

    upfate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 23
         // Target 36 to stay current with Play Store 2026 requirements
         targetSdk = 36
-        versionCode = 320
+        versionCode = 321
         versionName = "10.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
